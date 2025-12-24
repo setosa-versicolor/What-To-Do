32cb7364850b3c8752001ed9bae7b0ed0f16e1d0
setosa-versicolor <81941874+setosa-versicolor@users.noreply.github.com>
2025-12-23 21:56:58 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Mad Local</h1>
+      <p>If you can see this, GitHub Pages is working 🎉</p>
+    </main>
+  );
+}
